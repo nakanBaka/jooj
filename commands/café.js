edf022adac2954bfacef9cf4df085e6de9dd8ed5
Nakan <39820938+nakanBaka@users.noreply.github.com>
2018-07-01 21:14:00 -0300

    Delete café.js

--- commands/café.js
+++
@@ -1,9 +0,0 @@
-exports.run = (client,message,args) => {
-var nameResult = args.join(' ');
-if (!nameResult) nameResult = null;
-client.user.setActivity(nameResult, {type: "STREAMING"});
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(config.prefix.length);
- message.channel.send('droga! você descobriu, ok vou mudar para ${nameResult}')
-}
