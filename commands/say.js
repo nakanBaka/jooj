7b5339434883d6db9a25eb3d2a9578331083bab0
Nakan <39820938+nakanBaka@users.noreply.github.com>
2018-07-08 19:15:43 -0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,17 +1,3 @@
-const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
+module.exports.run = (bot, message, args) => {
 
-module.exports.run = async (bot, message, args) => {
-
-  message.delete();
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  let botmessage = args.join(" ");
-  message.channel.send(botmessage);
-}
-
-module.exports.help = {
-  name: "say"
-}
-
-
-
+  message.channel.send(args)
