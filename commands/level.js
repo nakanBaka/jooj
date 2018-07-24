b4885d54d7c637ad8c58393217d896c89db35528
Nakan <monstercat175@gmail.com>
2018-07-24 12:27:20 -0300

    Create level.js

---
+++ commands/level.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+let xp = require("../xp.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!xp[message.author.id]){
+   xp[message.author.id] = {
+     xp: 0,
+     level: 1
+  };
+}
+  let curxp = xp[message.author.id].xp;
+  let curlvl = xp[message.author.id].level;
+  let nxtLvlXp = curlvl * 300;
+  let difference = nxtLvlXp - curxp;
+
+  let lvlEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("RANDOM")
+  .addField("Level", curlvl, true)
+  .addField("XP", curxp, true)
+  .setFooter(`${difference} XP para subir de nível`, message.author.displayAvatarURL);
+
+  message.channel.send(lvlEmbed)
+
+}
+
+module.exports.help = {
+  name: "level"
+}
